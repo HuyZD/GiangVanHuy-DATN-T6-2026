ac2431880d07472dbd9536c10f49aebfc8ceed57
HuyZD <163296780+HuyZD@users.noreply.github.com>
2026-04-06 17:35:29 +0700

    6/4 part 2

--- Code/Firmware/src/main.cpp
+++ Code/Firmware/src/main.cpp
@@ -1,7 +1,15 @@
 #include <Arduino.h>
 #include "./IR_Receiver/IR_Receiver.h"
+#include "./IR_Sender/IR_Sender.h"
+#include "./PH_Sensor/PH_sensor.h"
+#include "./SCD40/SCD40.h"
+#include "./TDSSensor/TDSSensor.h"
+#include "./TSL2561/SparkFunTSL2561Example.h"
+#include "./RS485Sensor/RS485Sensor.h"  
+
 
 void setup() {
+  Serial.begin(9600);
   IR_receiver_setup();
 }
 
